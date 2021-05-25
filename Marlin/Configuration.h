5790d02f51060db1cf44fa9d072cfa8286fb144e
fuzzynm <fuzzynm@gmail.com>
2021-05-25 10:40:33 -0600

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1719,7 +1719,7 @@
 #define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
 
 #define PREHEAT_2_LABEL       "ABS"
-#define PREHEAT_2_TEMP_HOTEND 240
+#define PREHEAT_2_TEMP_HOTEND 225
 #define PREHEAT_2_TEMP_BED    110
 #define PREHEAT_2_TEMP_CHAMBER 35
 #define PREHEAT_2_FAN_SPEED     0 // Value from 0 to 255
